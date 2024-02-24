424890ed45ec1b61479fff00d456efdb548b3e66
Lorri Lanig <lorrilanig.art@gmail.com>
2024-02-24 13:29:16 -0600

    rover-completed

--- rover.js
+++ rover.js
@@ -10,52 +10,45 @@ class Rover {
          throw Error('values are required');
       }
    }
-   receiveMessage(message) {
-      //response returns the message.name
-      let results = [];
-      message = new Message(message);
-      //response returned includes two results if two commands are sent
-      
-      results = message.commands; 
-      
-      //responds correctly to status check command
-      if (this.mode === 'STATUS_CHECK') {
-         let roverStatus = new Rover(this.mode, this.generatorWatts, this.position);
-         results.push(roverStatus);
 
+   receiveMessage(message) {
+      let response = {
+         message: message.name,
+         results: []
       }
-      //responds correctly to mode change command
-      if (this.mode  === 'MODE_CHANGE') {
-         let completed = true; 
-         this.mode = Command.value; 
-         results.push(completed);
+
+      for (let i=0; i < message.commands.length; i++) {
+        
+         if (message.commands[i].commandType === 'MOVE') {
+            if (this.mode === 'LOW_POWER') {
+               response.results.push({completed: false});
+            } else {
+               response.results.push({completed: true});
+               this.position = message.commands[i].value;
+         }
+            }
+            else if (message.commands[i].commandType === 'MODE_CHANGE') {
+               response.results.push({completed: true});
+               this.mode = message.commands[i].value;
+          } else {
+            if (message.commands[i].commandType === 'STATUS_CHECK') {
+               response.results.push({
+                  completed: true, 
+                  roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}})
+               } 
+           
+ 
+         }
+         
       }
-      //responds with a false completed value when attempting to move in low power
-      if (this.mode === 'LOW_POWER') {
-         let completed = false;
-         results.push(completed);
-      }
-      if (this.mode === 'MOVE') {
-         Rover.position = this.position;
-      };
+      return response;}}
+
       
-      return {message: this.message, results: this.results}
+
       
-   }
-}
-let rover = new Rover(100);
-let message = new Message('TA power');
-console.log(rover.receiveMessage(message))
+
+
+
+
 
 module.exports = Rover;
-// let rover = new Rover(100);
-// let commands = [
-//    new Command('MOVE', 4321),
-//    new Command('STATUS_CHECK'),
-//    new Command('MODE_CHANGE', 'LOW_POWER'),
-//    new Command('MOVE', 3579),
-//    new Command('STATUS_CHECK')
-// ];
-// let message = new Message('TA power', commands);
-// let response = rover.receiveMessage(message);
-//console.log(JSON.stringify(response,null, 2));
